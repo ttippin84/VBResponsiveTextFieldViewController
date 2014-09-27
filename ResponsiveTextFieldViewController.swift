662fb5c537d9cdd1af7f35ca8ec5eed3e78fd762
dsandor <dsandor@macmini.devsql.local>
2014-09-27 15:07:43 -0500

    Version one of conversion.
    I converted ttippin84’s objective C version of this to swift and fixed
    a bug because the textfield was not setting the activeField value
    properly.

---
+++ ResponsiveTextFieldViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ResponsiveTextFieldViewController.swift
+//  Swift version of: VBResponsiveTextFieldViewController
+//  Original code: https://github.com/ttippin84/VBResponsiveTextFieldViewController
+//
+//  Created by David Sandor on 9/27/14.
+//  Copyright (c) 2014 David Sandor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResponsiveTextFieldViewController : UIViewController
+{
+
+    var kPreferredTextFieldToKeyboardOffset: CGFloat = 20.0
+    var keyboardFrame: CGRect = CGRect.nullRect
+    var keyboardIsShowing: Bool = false
+    weak var activeTextField: UITextField?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        for subview in self.view.subviews
+        {
+            if (subview.isKindOfClass(UITextField))
+            {
+                var textField = subview as UITextField
+                textField.addTarget(self, action: "textFieldDidReturn:", forControlEvents: UIControlEvents.EditingDidEndOnExit)
+                
+                textField.addTarget(self, action: "textFieldDidBeginEditing:", forControlEvents: UIControlEvents.EditingDidBegin)
+                
+            }
+        }
+    
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification)
+    {
+        self.keyboardIsShowing = true
+        
+        if let info = notification.userInfo {
+           self.keyboardFrame = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+           self.arrangeViewOffsetFromKeyboard()
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification)
+    {
+        self.keyboardIsShowing = false
+        
+        self.returnViewToInitialFrame()
+    }
+    
+    func arrangeViewOffsetFromKeyboard()
+    {
+        var theApp: UIApplication = UIApplication.sharedApplication()
+        var windowView: UIView? = theApp.delegate!.window!
+        
+        var textFieldLowerPoint: CGPoint = CGPointMake(self.activeTextField!.frame.origin.x, self.activeTextField!.frame.origin.y + self.activeTextField!.frame.size.height)
+        
+        var convertedTextFieldLowerPoint: CGPoint = self.view.convertPoint(textFieldLowerPoint, toView: windowView)
+        
+        var targetTextFieldLowerPoint: CGPoint = CGPointMake(self.activeTextField!.frame.origin.x, self.keyboardFrame.origin.y - kPreferredTextFieldToKeyboardOffset)
+        
+        var targetPointOffset: CGFloat = targetTextFieldLowerPoint.y - convertedTextFieldLowerPoint.y
+        var adjustedViewFrameCenter: CGPoint = CGPointMake(self.view.center.x, self.view.center.y + targetPointOffset)
+        
+        UIView.animateWithDuration(0.2, animations:  {
+          self.view.center = adjustedViewFrameCenter
+        })
+    }
+    
+    func returnViewToInitialFrame()
+    {
+        var initialViewRect: CGRect = CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height)
+        
+        if (!CGRectEqualToRect(initialViewRect, self.view.frame))
+        {
+            UIView.animateWithDuration(0.2, animations: {
+                self.view.frame = initialViewRect
+            });
+        }
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
+    {
+        if (self.activeTextField != nil)
+        {
+            self.activeTextField?.resignFirstResponder()
+            self.activeTextField = nil
+        }
+    }
+    
+    @IBAction func textFieldDidReturn(textField: UITextField!)
+    {
+        textField.resignFirstResponder()
+        self.activeTextField = nil
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField)
+    {
+        self.activeTextField = textField
+        
+        if(self.keyboardIsShowing)
+        {
+            self.arrangeViewOffsetFromKeyboard()
+        }
+    }
+}
